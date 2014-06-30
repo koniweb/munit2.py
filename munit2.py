e9f0e563559cb4987575656dd6119b34658ec462
koniweb <konstantin.weber@fau.de>
2014-06-30 13:28:07 +0200

    input read changed

--- munit2.py
+++ munit2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 ######################################################################
-# munit.py
+# munit.py                                                           #
 # script to read and write coordinates in various formats            #
 # to add: multiple xyz                                               #
 #--------------------------------------------------------------------#
@@ -13,8 +13,10 @@
 # 2013-12-11 Version 1.5  -- error in pwscf input fixed              #
 # 2013-12-11 Version 1.6  -- pwscf output readable                   #
 # 2014-06-26 Version 2.0  -- shift to molecule                       #
+# 2014-06-30 Version 2.1  -- allow to read data from pwscf           #
+#                            TODO: TEST                              #
 ######################################################################
-version="2.0"
+version="2.1"
 
 #----------------------------------------------------------------------
 # import
@@ -36,6 +38,7 @@ def main():
     out='xyz'
     inf='xyz'
     quiet=int(0);
+    datapwscf=""
     # vector, localvec, offset
     factor=[float(1.000),float(1.000),float(1.000)]
     localvec=[ [float(1.000),float(0.000),float(0.000)],
@@ -50,63 +53,49 @@ def main():
         print >>sys.stderr, 'no user arguments'
         stop()
     
-    arg=0
-    while ( (arg+1) < len(sys.argv)):
-        arg=arg + 1
-        if sys.argv[arg].startswith('--'):
-            option = sys.argv[arg][2:]
-            if   option == 'h':
-                start(version)
-                showhelp()
-            elif option == 'in':
-                inf=readopt(arg)
-                arg=arg+1
-            elif option == 'coo':
-                file_coord=readfilename(arg)
-                arg=arg+1
-            elif option == 'a':
-                a=[0.0,0.0,0.0]
-                readvec(a,arg)
-                localvec[0][:]=a
-                arg=arg+3
-            elif option == 'b':
-                b=[0.0,0.0,0.0]
-                readvec(b,arg)
-                localvec[1][:]=b
-                arg=arg+3
-            elif option == 'c':
-                c=[0.0,0.0,0.0]
-                readvec(c,arg)
-                localvec[2][:]=c
-                arg=arg+3
-            elif option == 'off':
-                offset=[0.0,0.0,0.0]
-                readvec(off,arg)
-                arg=arg+3
-            elif option == 'm':
-                readm(m,arg)
-                arg=arg+3
-            elif option == 'f':
-                factor[0]=readfactor(arg)
-                factor[1]=factor[0]
-                factor[2]=factor[0]
-                arg=arg+1
-            elif option == 'fxyz':
-                readvec(factor,arg)
-                arg=arg+3
-            elif option == 'out':
-                out=readopt(arg)
-                arg=arg+1
-            elif option == 'q':
-                quiet=1
-            else:
-                start(version)
-                print >>sys.stderr, "option not known"
-                stop()
+    # argument parsing
+    args=[]
+    A=" ".join(sys.argv).split("--")
+    for i in range(1,len(A)): args.append(A[i].strip().split(" "))
+
+    # go through list
+    for arg in args:
+        if   arg[0]=="h":
+            start(version)
+            showhelp()
+        # input files
+        elif arg[0]=='coo':
+            file_coord=readfilename(arg)
+        elif arg[0]=='datapwscf':
+            datapwscf=readfilename(arg)
+        # file types
+        elif arg[0]=='in':
+            inf=readin(arg)
+        elif arg[0]=='out':
+            out=readout(arg)
+        # vectors
+        elif arg[0]=='a':
+            localvec[0][:]=readvec(arg,localvec[0][:],'a',0)
+        elif arg[0]=='b':
+            localvec[1][:]=readvec(arg,localvec[1][:],'b',1)
+        elif arg[0]=='c':
+            localvec[2][:]=readvec(arg,localvec[2][:],'c',2)
+        elif arg[0]=='off':
+            offset=readvec(arg,offset,'offset')
+        # stretch and multiply options
+        elif arg[0]=='f':
+            f=readfactor(arg)
+        elif arg[0]=='m':
+            m=readvecint(arg,m,'m')
+        # quiet
+        elif arg[0]=='q':
+            quiet=1
+        # else
         else:
             start(version)
-            print >>sys.stderr, 'command ', sys.argv[arg], ' not known'
+            print >>sys.stderr, "option ",arg[0]," not known"
             stop()
+
     #-- START PROGRAM PROMPT ----------------------------------------------
     if quiet == 0:
         start(version)
@@ -115,6 +104,12 @@ def main():
 
     #-- READ COORDINATE FILES ---------------------------------------------
     mol=readinfo(inf,file_coord)
+    
+    # read data from pwscf file
+    if not datapwscf=="":
+        mol.setup_pwscf=mol.SETUP_PWSCF()
+        mol.read_setup_pwscf(datapwscf)
+                                       
 
     # add vec and offset
     v=mol.Rvec()
@@ -139,61 +134,78 @@ def main():
 #----------------------------------------------------------------------
 # Functions
 #----------------------------------------------------------------------
-# read vector
-def readvec(vector,argument):
-    if ((argument+1+3) > len(sys.argv)):
-        print >>sys.stderr, 'vectors not given completly'
+# read vector either all directions or only in direction dir
+def readvec(arg,vector,name="",dir=-1):
+    if len(arg)==2: 
+        vector=[0.0,0.0,0.0]
+        vector[dir]=float(arg[1])
+    elif len(arg)==4: vector=[float(arg[1]),float(arg[2]),float(arg[3])]
+    else:
+        print >>sys.stderr, 'vector ',name,' not given completly'
+        stop()
+    return vector
+def readvecint(arg,vector,name=""):
+    if len(arg) < 4:
+        print >>sys.stderr, 'vector ',name,' not given completly'
         stop()
     else:
-        vector[0]=float(sys.argv[argument+1])
-        vector[1]=float(sys.argv[argument+2])
-        vector[2]=float(sys.argv[argument+3])
+        vector[0]=int(arg[1])
+        vector[1]=int(arg[2])
+        vector[2]=int(arg[3])
+    return vector
 
 # read filename
 def readfilename(argument):
-    if ((argument+1+1) > len(sys.argv)):
-        print  >>sys.stderr, 'vector not given completly'
+    if len(argument) < 2:
+        print  >>sys.stderr, 'file not given'
         stop()
-    else:
-        return sys.argv[argument+1]
-
-# readm
-def readm(m,argument):
-    if ((argument+1+3) > len(sys.argv)):
-        print  >>sys.stderr, 'm not given completly'
-        stop()
-    else:
-        m[0]=int(sys.argv[argument+1]) #mx
-        m[1]=int(sys.argv[argument+2]) #my
-        m[2]=int(sys.argv[argument+3]) #mz
+    return argument[1]
 
 # read factor
-def readfactor(argument):
-    if ((argument+1+1) > len(sys.argv)):
+def readfactor(arg):
+    if   len(argu) == 2:
+        factor=[float(arg[1]),float(arg[1]),float(arg[1])]
+    elif len(argument) == 4:
+        factor=[float(arg[1]),float(arg[2]),float(arg[3])]
+    else:
         print  >>sys.stderr, 'factor not given completly'
         stop()
-    else:
-        return float(sys.argv[argument+1]) 
-        print  >>sys.stderr, 'subrout', float(sys.argv[argument+1]) 
+    return factor
 
 # reading options
-def readopt(argument):
-    if ((argument+1+1) > len(sys.argv)):
-        print  >>sys.stderr, 'output or input type not given completly'
+def readin(arg):
+    if  len(arg) < 2:
+        print  >>sys.stderr, 'input type not given completly'
         stop()
     else:
-        if   (sys.argv[argument+1] == 'xyz'):
+        print arg
+        if   (arg[1] == 'xyz'):
             return 'xyz'
-        elif (sys.argv[argument+1] == 'lammps'):
+        elif (arg[1] == 'lammps'):
             return 'lammps'
-        elif (sys.argv[argument+1] == 'pwscfin'):
+        elif (arg[1] == 'pwscfin'):
             return 'pwscfin'
-        elif (sys.argv[argument+1] == 'pwscfout'):
+        elif (arg[1] == 'pwscfout'):
             return 'pwscfout'
-        elif (sys.argv[argument+1] == 'pwscf'):
+        else:
+            print  >>sys.stderr, 'input option not known'
+            stop()
+        
+
+# reading options
+def readout(arg):
+    if  len(arg)< 2:
+        print  >>sys.stderr, 'output type not given completly'
+        stop()
+    else:
+        if   (arg[1] == 'xyz'):
+            return 'xyz'
+        elif (arg[1] == 'lammps'):
+            return 'lammps'
+        elif (arg[1] == 'pwscf'):
             return 'pwscf'
         else:
-            print  >>sys.stderr, 'out- or input option not known'
+            print  >>sys.stderr, 'output option not known'
             stop()
 
 # printing info
@@ -265,28 +277,28 @@ def stop():
     sys.exit()
 def showhelp():
     print >>sys.stderr, 'to invoke munit2.py you have the following options:'
-    print >>sys.stderr, '--h                   show this help'
-    print >>sys.stderr, '--q                   quiet output'
-    print >>sys.stderr, '--coo  <coord>        show coordinate file'
-    print >>sys.stderr, '--a    <x> <y> <z>    x, y, z components from unitvector a'
-    print >>sys.stderr, '--b    <x> <y> <z>    x, y, z components from unitvector b'
-    print >>sys.stderr, '--c    <x> <y> <z>    x, y, z components from unitvector c'
-    print >>sys.stderr, '--off  <x> <y> <z>    x, y, z offset of unitvectors'
-    print >>sys.stderr, '--f    <factor>       stretch factor for the unitcell '
-    print >>sys.stderr, '--fxyz <x> <y> <z>    stretch factor for the unitcell '
-    print >>sys.stderr, '--m    <x> <y> <z>    multiplication in x, y, z direction'
-    print >>sys.stderr, '--out  <option>       option for output'
-    print >>sys.stderr, '                        xyz -- standard (no multiplexyz files)'
-    print >>sys.stderr, '                        lammps'
-    print >>sys.stderr, '                        pwscf'
-    print >>sys.stderr, '--in   <option>       option for input'
-    print >>sys.stderr, '                        xyz -- standard'
-    print >>sys.stderr, '                        lammps'
-    print >>sys.stderr, '                        pwscfin'
-    print >>sys.stderr, '                        pwscfout'
+    print >>sys.stderr, '--h                      show this help'
+    print >>sys.stderr, '--q                      quiet output'
+    print >>sys.stderr, '--coo  <coord>           show coordinate file'
+    print >>sys.stderr, '--a    <x>  [<y>   <z>]  x, y, z components from unitvector a'
+    print >>sys.stderr, '--b   [<x>]  <y>  [<z>]  x, y, z components from unitvector b'
+    print >>sys.stderr, '--c   [<x>   <y>]  <z>   x, y, z components from unitvector c'
+    print >>sys.stderr, '--off  <x>   <y>   <z>   x, y, z offset of unitvectors'
+    print >>sys.stderr, '--f    <a>  [<b>   <c>]  stretch factor for the unitcell '
+    print >>sys.stderr, '--m    <x>   <y>   <z>   multiplication in x, y, z direction'
+    print >>sys.stderr, '--out  <option>          option for output'
+    print >>sys.stderr, '                           xyz -- standard'
+    print >>sys.stderr, '                           lammps'
+    print >>sys.stderr, '                           pwscf'
+    print >>sys.stderr, '--in   <option>          option for input'
+    print >>sys.stderr, '                           xyz -- standard'
+    print >>sys.stderr, '                           lammps'
+    print >>sys.stderr, '                           pwscfin'
+    print >>sys.stderr, '                           pwscfout'
+    print >>sys.stderr, '--datapwscf <file>       file to read pwscf simulation setup'
     print >>sys.stderr, ''
     print >>sys.stderr, 'example:'
-    print >>sys.stderr, 'munit2.py --coo test.xyz --a 1 0 0 --b 0 1 0 --c 0 0 1 --m 1 1 1 '
+    print >>sys.stderr, 'munit2.py --coo test.xyz --a 1 --b 0 1 0 --c 0 0 1 --m 1 1 1 '
     sys.exit()
 
 
